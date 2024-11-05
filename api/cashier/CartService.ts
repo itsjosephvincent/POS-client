546f609e1ea14951ec38a7da02fd2865b76d84b9
Joseph <josephvincentlimbaroc@gmail.com>
2024-11-05 09:15:52 +0800

    updated cart service

--- api/cashier/CartService.ts
+++ api/cashier/CartService.ts
@@ -9,21 +9,21 @@ export class CartService extends BaseAPIService {
         }
         return CartService.instance;
     }
+
     async fetch(params: object | null): Promise<any> {
         return await this.request(`/cashier/carts`, 'GET', params);
     }
+
     async find(id: string | string[], params: object) {
         return await this.request(`/cashier/carts/${id}`, 'GET', params);
     }
+
     async create(params: object) {
         return await this.request(`/cashier/carts`, 'POST', params);
     }
+
     async void(params: object, uuid: string) {
-        return await this.request(
-            `/cashier/carts/${uuid}?_method=PUT`,
-            'POST',
-            params,
-        );
+        return await this.request(`/cashier/carts/${uuid}`, 'DELETE', params);
     }
 }
 
